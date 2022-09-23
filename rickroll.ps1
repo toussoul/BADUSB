2f7eb3fb34506c406332b4e311148708ba16b198
thomas oussoul <thomasoussoult@gmail.com>
2022-09-23 23:43:47 +0200

    add rickroll

---
+++ rickroll.ps1
@@ -0,0 +1,1 @@
+$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New(1); $MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://www.cjoint.com/doc/16_09/FIsxS52QXY7_Rick-Astley---Never-Gonna-Give-You-Up.mp3'); $MediaPlayer.Play(); while(1){(new-object -com wscript.shell).SendKeys([char]175);}
