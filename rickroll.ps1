951aa23f329f8cef4882c2f4c3ce7e251f6a17de
toussoul <48627249+toussoul@users.noreply.github.com>
2022-09-23 23:50:55 +0200

    Update rickroll.ps1

--- rickroll.ps1
+++ rickroll.ps1
@@ -1,4 +1,6 @@
-$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
-$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://www.cjoint.com/doc/16_09/FIsxS52QXY7_Rick-Astley---Never-Gonna-Give-You-Up.mp3')
-$MediaPlayer.Play()
-while(1){(new-object -com wscript.shell).SendKeys([char]175);}
+$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New();
+$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://www.cjoint.com/doc/16_09/FIsxS52QXY7_Rick-Astley---Never-Gonna-Give-You-Up.mp3');
+$MediaPlayer.Play();
+while(1){
+  (new-object -com wscript.shell).SendKeys([char]175);
+}
